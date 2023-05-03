fd70562d3953b647a4e7e5f17c349b84012d6540
Himani Joshi <himanijoshi28@gmail.com>
2023-05-03 11:06:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+     backgroundImage: {
+        "bg": "url('./images/bg.png')",
+      
+      },
+    },
+  },
+  plugins: [],
+}
+
